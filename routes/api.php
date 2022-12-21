a67132da98e0b442926a239356e44a89a870a171
Haady Raja <39164862+haadiraja@users.noreply.github.com>
2022-12-21 20:47:51 +0500

    jglkfdlgkfj

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Posts;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/posts', function(){
+
+    $posts = Posts::all();
+
+    return $posts;
+
+});
+
+
+Route::post('/post', function(){
+
+    $post = new Posts();
+
+    $post->title = request('title');
+    $post->content = request('content');
+    $post->save();
+    return $post;
+
+
 });
